b459567d323f3567d56a3251d982029dd293fd63
ralph-jo <ralph.jo@kakaopiccoma.com>
2025-08-03 23:09:03 +0900

    feat: 접근 URL 설정

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/dizwe_com">
       <div className="app">
         <Header />
         <main className="main-content">
